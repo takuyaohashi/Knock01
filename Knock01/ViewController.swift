684e518abf05e6ca59403ee619737db276d82b7a
Takuya OHASHI <tky.ohashi@gmail.com>
2017-04-26 00:01:12 +0900

    Initial Commit

---
+++ Knock01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Knock01
+//
+//  Created by Takuya OHASHI on 2017/04/26.
+//  Copyright © 2017年 Takuya OHASHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
