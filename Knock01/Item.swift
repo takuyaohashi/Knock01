10cc3dfeef428358f1bf02fc4fb9b241f39938c4
Takuya OHASHI <tky.ohashi@gmail.com>
2017-05-03 22:28:52 +0900

    Item クラスを定義
     - Realm で管理するように Object を継承

--- Knock01/Item.swift
+++ Knock01/Item.swift
@@ -5,5 +5,11 @@
 //  Created by Takuya OHASHI on 2017/04/30.
 //  Copyright © 2017年 Takuya OHASHI. All rights reserved.
 //
+import RealmSwift
 
-import Foundation
+class Item: Object {
+    dynamic var title = ""
+    dynamic var desc: String? = nil
+    dynamic var date = NSDate()
+    dynamic var done = false
+}
