f5c39e13d9f9a07bf604b28393c62074b2c7bf9a
Takuya OHASHI <tky.ohashi@gmail.com>
2017-05-06 11:30:05 +0900

    RealmHelper を追加
     - object を取り出すヘルパ

---
+++ Knock01/RealmHelper.swift
@@ -0,0 +1,16 @@
+//
+//  RealmHelper.swift
+//  Knock01
+//
+//  Created by Takuya OHASHI on 2017/05/06.
+//  Copyright © 2017年 Takuya OHASHI. All rights reserved.
+//
+
+import RealmSwift
+
+class RealmHelper {
+    static func objects<T: Object>(type: T.Type) -> Results<T>? {
+        let realm = try? Realm()
+        return realm?.objects(type)
+    }
+}
