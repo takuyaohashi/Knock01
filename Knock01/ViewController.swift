0af22b9f84b6247e1e79bd2cfb35383f040c5e90
Takuya OHASHI <tky.ohashi@gmail.com>
2017-06-24 11:02:55 +0900

    Realm Object Server を使用するように修正
     - 下記のユーザがいることが前提
     -- user: hoge@example.com
     -- password: hogehoge

--- Knock01/ViewController.swift
+++ Knock01/ViewController.swift
@@ -8,14 +8,47 @@
 
 import UIKit
 import RealmSwift
+import Dispatch
 
 class ViewController: UIViewController {
+    var notificationToken: NotificationToken!
+    var realm: Realm!
     var items = List<Item>()
+
+    @IBOutlet weak var tableView: UITableView!
     var todolist = RealmHelper.objects(type: Item.self)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "ToDo List"
+        let username = "hoge@example.com"
+        let password = "hogehoge"
+
+        SyncUser.logIn(with: .usernamePassword(username: username, password: password), server: URL(string: "http://127.0.0.1:9080")!) { user, error in
+            guard let user = user else {
+                fatalError(String(describing: error))
+            }
+            DispatchQueue.main.async {
+                let configuration = Realm.Configuration(
+                    syncConfiguration: SyncConfiguration(user: user, realmURL: URL(string: "realm://127.0.0.1:9080/~/realmtasks")!)
+                )
+                self.realm = try! Realm(configuration: configuration)
+
+                // Show initial tasks
+                func updateList() {
+                    if self.items.realm == nil, let list = self.realm.objects(ItemList.self).first {
+                        self.items = list.items
+                    }
+                    self.tableView.reloadData()
+                }
+                updateList()
+
+                // Notify us when Realm changes
+                self.notificationToken = self.realm.addNotificationBlock { _ in
+                    updateList()
+                }
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -23,6 +56,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    deinit {
+        self.notificationToken.stop()
+    }
 
     /*
     // MARK: - Navigation
@@ -40,6 +76,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
         let item = items[indexPath.row]
