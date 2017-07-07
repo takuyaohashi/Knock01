886bba2fcdae4cac889ec67f9b18c6cea2ef1e35
Takuya OHASHI <tky.ohashi@gmail.com>
2017-07-08 00:25:24 +0900

    TableView を追加
     - あれ、こうするなら UIViewController じゃなくて
       UITableViewController を継承した方がいいのではないか…？

--- Knock02/Knock02/ViewController.swift
+++ Knock02/Knock02/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 画面いっぱいの tableView の生成
+        tableView = UITableView(frame: UIScreen.main.bounds)
+
+        self.view.addSubview(tableView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
