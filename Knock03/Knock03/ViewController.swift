db260b8e17531b83f11721130708bff265bc954a
Takuya OHASHI <tky.ohashi@gmail.com>
2017-07-11 23:47:21 +0900

    とりあえず UICollectionView を描画
     - navigationController も使う

--- Knock03/Knock03/ViewController.swift
+++ Knock03/Knock03/ViewController.swift
@@ -10,16 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var calendarView : UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "calendar"
+
+        createCalendar()
+    }
+    
+    func createCalendar() {
+        calendarView = UICollectionView(frame: UIScreen.main.nativeBounds,
+                                        collectionViewLayout: UICollectionViewLayout())
+        calendarView.backgroundColor = UIColor.lightGray
+        self.view.addSubview(calendarView)
+        
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+
+        self.view.addConstraints([
+        
+            NSLayoutConstraint(item: calendarView,
+                               attribute: .top,
+                               relatedBy: .equal,
+                               toItem: self.topLayoutGuide,
+                               attribute: .bottom,
+                               multiplier: 1.0,
+                               constant: 0),
+
+            NSLayoutConstraint(item: calendarView,
+                               attribute: .width,
+                               relatedBy: .equal,
+                               toItem: self.view,
+                               attribute: .width,
+                               multiplier: 1.0,
+                               constant: 0),
+            
+            NSLayoutConstraint(item: calendarView,
+                               attribute: .bottom,
+                               relatedBy: .equal,
+                               toItem: self.view,
+                               attribute: .bottom,
+                               multiplier: 1.0,
+                               constant: 0),
+            
+        ]);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+//
+//extension ViewController: UICollectionViewDataSource {
+
+    
+
+//}
