fbc9e69d1d7ebb55b98c92608e6684d1ea4f255a
Takuya OHASHI <tky.ohashi@gmail.com>
2017-07-22 14:02:00 +0900

    CollectionView にセルを表示するように修正
     - とりあえず適当に並べてるだけ

--- Knock03/Knock03/ViewController.swift
+++ Knock03/Knock03/ViewController.swift
@@ -62,6 +62,7 @@ class ViewController: UIViewController {
         ]);
         
         calendarView.dataSource = self
+        calendarView.delegate   = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -73,11 +74,22 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.white
         return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 30, height: 30)
     }
 }
