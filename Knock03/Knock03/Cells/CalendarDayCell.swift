c174aae0625b8ef479ccad6b65a2cdd5e46a59ba
Takuya OHASHI <tky.ohashi@gmail.com>
2017-07-25 23:18:27 +0900

    セルをカスタム
     - とりあえず UILabel を追加しただけ

--- Knock03/Knock03/Cells/CalendarDayCell.swift
+++ Knock03/Knock03/Cells/CalendarDayCell.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 class CalendarDayCell: UICollectionViewCell {
+    var day : UILabel!
     
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        createDayLabel()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createDayLabel()
+    }
+
+    func createDayLabel() {
+        day = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+        day.font = UIFont(name: "HiraKakuProN-W3", size: 12)
+        day.textAlignment = .center
+
+        self.addSubview(day)
+    }
 }
--- Knock03/Knock03/Cells/CalendarWeekCell.swift
+++ Knock03/Knock03/Cells/CalendarWeekCell.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 class CalendarWeekCell: UICollectionViewCell {
+    var week : UILabel!
     
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        createWeekLabel()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createWeekLabel()
+    }
+
+    func createWeekLabel() {
+        week = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+        week.font = UIFont(name: "HiraKakuProN-W3", size: 12)
+        week.textAlignment = .center
+
+        self.addSubview(week)
+    }
 }
