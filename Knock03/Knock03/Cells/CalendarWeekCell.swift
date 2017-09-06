3998746157b6077c6d1f592811376e4b77c0741d
Takuya OHASHI <tky.ohashi@gmail.com>
2017-09-06 22:44:47 +0900

    use calendar.weekdaySymbol to get weekday name

--- Knock03/Knock03/Cells/CalendarWeekCell.swift
+++ Knock03/Knock03/Cells/CalendarWeekCell.swift
@@ -35,15 +35,8 @@ class CalendarWeekCell: UICollectionViewCell {
         case saturday
 
         var name : String {
-            switch self {
-            case .sunday:    return "Sun"
-            case .monday:    return "Mon"
-            case .tuesday:   return "Tue"
-            case .wednesday: return "Wed"
-            case .thursday:  return "Thu"
-            case .friday:    return "Fri"
-            case .saturday:  return "Sat"
-            }
+            let calendar = Calendar(identifier: .gregorian)
+            return calendar.weekdaySymbols[self.hashValue]
         }
     }
 
