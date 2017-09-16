91ea69632afa28085fba1317dc311dbc2d9fb0d0
Takuya OHASHI <tky.ohashi@gmail.com>
2017-09-16 16:01:47 +0900

    add Knock04

---
+++ Knock04/Knock04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Knock04
+//
+//  Created by Takuya OHASHI on 2017/09/16.
+//  Copyright © 2017年 Takuya OHASHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
