88319f95280844a897b7ac816c01b81b590c1da9
Takuya OHASHI <tky.ohashi@gmail.com>
2017-09-16 23:25:02 +0900

    display calendar

--- Knock04/Knock04/ViewController.swift
+++ Knock04/Knock04/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class ViewController: UIViewController {
+    let formatter = DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,3 +25,23 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale   = Calendar.current.locale
+
+        let startDate = formatter.date(from: "2017 01 01")!
+        let endDate = formatter.date(from: "2017 12 31")!
+
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CutomCell", for: indexPath) as! CustomCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+}
