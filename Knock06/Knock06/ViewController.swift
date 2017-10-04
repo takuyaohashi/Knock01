be00d9effaba78dbbb660f8633a27ec0e5651bf4
Takuya OHASHI <tky.ohashi@gmail.com>
2017-10-04 21:35:35 +0900

    add Knock06

---
+++ Knock06/Knock06/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Knock06
+//
+//  Created by Takuya OHASHI on 2017/10/04.
+//  Copyright © 2017年 Takuya OHASHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
