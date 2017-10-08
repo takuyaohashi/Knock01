6caa9806a8cadcaf229ddb333fd9a76964c3642e
Takuya OHASHI <tky.ohashi@gmail.com>
2017-10-08 18:40:08 +0900

    connect UI to code

--- Knock06/Knock06/ViewController.swift
+++ Knock06/Knock06/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var emailText: UITextField!
+    @IBOutlet var passwordText: UITextField!
+    @IBOutlet var signupButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
